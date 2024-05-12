351604344c92f9c73ad1217fcd245693e35bd881
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-05-12 12:49:19 +0300

    Итераторы.py

--- Итераторы.py
+++ Итераторы.py
@@ -2,20 +2,18 @@ class EvenNumbers:
     def __init__(self, start=0, end=1):
         self.start = start
         self.end = end
+        self.current = self.start
 
     def __iter__(self):
-        self.current = self.start
         return self
 
     def __next__(self):
-        if self.current > self.end or self.current % 2 != 0:
+        if self.current > self.end:
             raise StopIteration
-        else:
-            result = self.current
-            self.current += 2
-            return result
+        result = self.current
+        self.current += 2
+        return result
 
-# Пример использования
-en = EvenNumbers(10, 25)
+en = EvenNumbers(9, 25)
 for i in en:
     print(i)
