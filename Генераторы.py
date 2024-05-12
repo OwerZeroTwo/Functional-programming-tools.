2b20fd25bcf7bd39322921b1712ab51d95a8e621
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-05-12 13:07:49 +0300

    Генераторы.py

--- Генераторы.py
+++ Генераторы.py
@@ -1,16 +1,8 @@
 def all_variants(s):
-    def subsequence(s, i, result):
-        if i == len(s):
-            yield ''.join(result)
-        else:
-            subsequence(s, i + 1, result)
-            result.append(s[i])
-            yield from subsequence(s, i + 1, result)
-            result.pop()
+    for i in range(len(s) + 1):
+        for j in range(i, len(s) + 1):
+            yield s[i:j]
 
-    yield from subsequence(s, 0, [])
-
-# Пример использования функции
 a = all_variants("abc")
 for i in a:
     print(i)
